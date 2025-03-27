7d8bdcbeeee2d1a722d06583e2e47f7d29b20ca6
tomvhli <tomvhl@proton.me>
2025-03-27 10:48:19 +0700

    Day 1: Learned Python basics

---
+++ day1.py
@@ -0,0 +1,11 @@
+# Print "Hello World"
+print("Hello World")
+
+# Variable and math
+age = 25
+future_age = age + 10
+print(f"In 10 years, you'll be {future_age} years old.") 
+
+# Loop: Print number 1 - 5
+for i in range(1, 6):
+    print(i)
